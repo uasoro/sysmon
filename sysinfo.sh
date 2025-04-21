6a9e8682f1e24fc9005f51fe14916aeb3424d6a4
hack3duasoro <155655800+uasoro@users.noreply.github.com>
2025-04-21 21:40:45 +0700

    delete version 0.1
    delete version 0.1 to update to 0.1.1.

--- sysinfo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo -e "\n \e[36m===================\n =   system info   =\n ===================\e[0m\n"
-
-echo -e "\e[33m| uptime:\e[0m $(uptime -p | sed 's/up //')"
-echo -e "\e[33m| cpu: \e[0m   $(top -bn1 | grep "%Cpu(s)" | awk '{print $2 + $4 "%"}')   |   $(cat /sys/class/thermal/thermal_zone*/temp | awk '{print $1 / 1000 "°C"}') "
-echo -e "\e[33m| external IP: \e[0m $(curl -s --max-time 3 ifconfig.me || echo "Disconnect")"
-echo -e "\e[33m| ram:\e[0m $(free -m | awk '/Mem:/ {printf "%.1f%% (%d/%d MB)", ($3/$2)*100, $3, $2}')"
-echo -e "\e[33m| user:\e[0m $(whoami)"
